f6d7abc05fd35ee8c29815cea6b2df80e7d651c4
Alireza <alirezacraft@gmail.com>
2021-02-24 22:45:40 +0330

    New Test Project

---
+++ t1.py
@@ -0,0 +1,22 @@
+# Type your code here
+def my_func(a,b):
+    if a>=0 and b>=b and b%2==0:
+        x=int(2*a-b/2)
+        y=int(a-x)
+        if x>0 and y>0:
+            result_=[x,y]
+            return result_
+        else:
+            result_='None'
+            return result_
+    else:
+        result_='None'
+        return result_
+
+ 
+
+
+x=int(input('Enter Num 1st:'))
+y=int(input('Enter Num 2nd:'))
+print(my_func(x,y))
+    
